eca690c69bb78406ea817fc6fd9c146a86b293bc
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-04-22 11:38:34 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -23,7 +23,7 @@ if ! flatpak remotes | grep -q flathub; then
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
-echo "Installing Discord..."
-flatpak install -y flathub com.discordapp.Discord
+echo "Installing Vesktop..."
+flatpak install -y vesktop
 
 echo "Installation complete!"
