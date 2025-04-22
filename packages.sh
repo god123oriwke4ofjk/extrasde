000e66777f2d04050f2af9e24e0bec0b4bc13e05
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-04-22 11:35:47 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if ! command -v yay &> /dev/null; then
+    echo "yay is not installed. Installing yay..."
+    sudo pacman -Syu --noconfirm git base-devel
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd -
+    rm -rf /tmp/yay
+fi
+
+echo "Installing Brave..."
+yay -S --noconfirm brave-bin
+
 if ! command -v flatpak &> /dev/null; then
     echo "Flatpak is not installed. Installing flatpak..."
     sudo pacman -Syu --noconfirm flatpak
