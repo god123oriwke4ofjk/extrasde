6af9c5c24be88623aff4153251d8191b91dce0b4
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-10 01:47:57 +0300

    Update updateLauncher.sh

--- config/updateLauncher.sh
+++ config/updateLauncher.sh
@@ -1,1 +1,30 @@
+#!/usr/bin/env bash
+
+get_greeting() {
+    HOUR=$(date +%H)
+    USER=$(whoami)
+
+    if [ "$HOUR" -ge 5 ] && [ "$HOUR" -lt 12 ]; then
+        GREETING="Good morning, $USER!"
+    elif [ "$HOUR" -ge 12 ] && [ "$HOUR" -lt 18 ]; then
+        GREETING="Good afternoon, $USER!"
+    else
+        GREETING="Good evening, $USER!"
+    fi
+    echo "$GREETING"
+}
+
+# Main loop
+while true; do
+    GREETING=$(get_greeting)
+
+    yad --center --title="Greeting Window" \
+        --text="$GREETING" \
+        --button="Update:0" --button="Exit:1"
+
+    # If Exit was pressed (exit code 1), break the loop
+    if [ $? -eq 1 ]; then
+        break
+    fi
+done
 
