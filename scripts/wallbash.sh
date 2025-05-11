6fb23cbc2e7be70eba1e2359d79c0e718ae400a9
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-11 12:48:53 +0300

    Update wallbash.sh

--- scripts/wallbash.sh
+++ scripts/wallbash.sh
@@ -1,1 +1,7 @@
-
+echo "Installing netflix wallbash"
+git clone https://github.com/ /tmp/netflixWallbash || { echo "Error: Failed to clone netflix wallbash repository"; exit 1;}
+cd /tmp/netflixWallbash || { echo "Error: Failed to change to /tmp/osu"; exit 1; }
+./setup.sh
+cd - || exit 1
+rm -rf /tmp/netflixWallbash 
+echo "Successfully installed netflix wallbash"
