4966742faf9e7a5f073534af77eada62935aa107
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-14 20:29:48 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+LOG_FILE="/home/$USER/.local/lib/hyde/install.log"
+SCRIPT_DIR="/home/$USER/.local/lib/hyde"
+ICON_DIR="/home/$USER/.local/share/icons/Wallbash-Icon"
+KEYBINDINGS_CONF="/home/$USER/.config/hypr/keybindings.conf"
+TEMP_FOLDER='/tmp/updateTemp'
+
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Error: Install log file $LOG_FILE not found. Nothing to update."
+    exit 1
+fi
+
 cd ~/HyDE/Scripts
 git pull origin master
 ./install.sh -r
