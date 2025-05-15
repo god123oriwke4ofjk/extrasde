62de3c4edfcb6070507f571fbae989c76d20fcd9
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-16 01:33:39 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,10 +5,18 @@ SCRIPT_DIR="/home/$USER/.local/lib/hyde"
 ICON_DIR="/home/$USER/.local/share/icons/Wallbash-Icon"
 KEYBINDINGS_CONF="/home/$USER/.config/hypr/keybindings.conf"
 TEMP_FOLDER='/tmp/updateTemp'
+BACKUP_DIR="/home/$USER/.local/lib/hyde/backups"
+
+mkdir $TEMP_FOLDER
 
 if [ ! -f "$LOG_FILE" ]; then
     echo "Error: Install log file $LOG_FILE not found. Nothing to update."
     exit 1
+fi
+
+if grep -q "MODIFIED_KEYBINDINGS:" "$LOG_FILE"; then
+    echo "Found modifed keybinds file, setting it up for update."
+    mv "$BACKUP_DIR/keybindings.conf.bak" ~/.config/hypr/keybindings.conf
 fi
 
 cd ~/HyDE/Scripts
@@ -18,3 +26,5 @@ git pull origin master
 cd ~/Extra
 git pull
 ./install.sh
+
+rm -rf $TEMP_FOLDER
