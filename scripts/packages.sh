81e287eb0bb84a8ca6592a369f9dd940fb2c405a
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-19 23:00:21 +0300

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -39,6 +39,7 @@ hydectl theme import --name "Oxo Carbon" -url https://github.com/rishav12s/Oxo-C
 hydectl theme import --name "Crimson Blue" -url https://github.com/amit-0i/Crimson-Blue
 hydectl theme import --name "Eternal Arctic" -url https://github.com/rishav12s/Eternal-Arctic
 hydectl theme import --name "Vanta Black" -url https://github.com/rishav12s/Vanta-Black
+hydectl theme import --name "Obsidian Purple" -url https://github.com/amit-0i/Obsidian-Purple
 
 [ "$EUID" -eq 0 ] && { echo "Error: This script must not be run as root."; exit 1; }
 
