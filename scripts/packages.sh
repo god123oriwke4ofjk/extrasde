8b267ba04b3e4a25d0d11f2db9f1fd5991a1ae32
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-19 23:01:27 +0300

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -35,11 +35,11 @@ for arg in "$@"; do
     esac
 done
 
-hydectl theme import --name "Oxo Carbon" -url https://github.com/rishav12s/Oxo-Carbon
-hydectl theme import --name "Crimson Blue" -url https://github.com/amit-0i/Crimson-Blue
-hydectl theme import --name "Eternal Arctic" -url https://github.com/rishav12s/Eternal-Arctic
-hydectl theme import --name "Vanta Black" -url https://github.com/rishav12s/Vanta-Black
-hydectl theme import --name "Obsidian Purple" -url https://github.com/amit-0i/Obsidian-Purple
+hydectl theme import --name "Oxo Carbon" --url https://github.com/rishav12s/Oxo-Carbon
+hydectl theme import --name "Crimson Blue" --url https://github.com/amit-0i/Crimson-Blue
+hydectl theme import --name "Eternal Arctic" --url https://github.com/rishav12s/Eternal-Arctic
+hydectl theme import --name "Vanta Black" --url https://github.com/rishav12s/Vanta-Black
+hydectl theme import --name "Obsidian Purple" --url https://github.com/amit-0i/Obsidian-Purple
 
 [ "$EUID" -eq 0 ] && { echo "Error: This script must not be run as root."; exit 1; }
 
