ee1d10abd5057399107ed25ed232d6cae45dde76
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-20 20:08:29 +0300

    Update themes.sh

--- scripts/themes.sh
+++ scripts/themes.sh
@@ -1,1 +1,5 @@
-
+hydectl theme import --name "Oxo Carbon" --url https://github.com/rishav12s/Oxo-Carbon
+hydectl theme import --name "Crimson Blue" --url https://github.com/amit-0i/Crimson-Blue
+hydectl theme import --name "Eternal Arctic" --url https://github.com/rishav12s/Eternal-Arctic
+hydectl theme import --name "Vanta Black" --url https://github.com/rishav12s/Vanta-Black
+hydectl theme import --name "Obsidian Purple" --url https://github.com/amit-0i/Obsidian-Purple
