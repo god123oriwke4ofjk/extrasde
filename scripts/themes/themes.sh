9e04ae683c2a820cf1bd0255e743e4e73802b18e
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-23 00:28:09 +0300

    Update themes.sh

--- scripts/themes/themes.sh
+++ scripts/themes/themes.sh
@@ -1,5 +1,36 @@
-hydectl theme import --name "Oxo Carbon" --url https://github.com/rishav12s/Oxo-Carbon
-hydectl theme import --name "Crimson Blue" --url https://github.com/amit-0i/Crimson-Blue
-hydectl theme import --name "Eternal Arctic" --url https://github.com/rishav12s/Eternal-Arctic
-hydectl theme import --name "Vanta Black" --url https://github.com/rishav12s/Vanta-Black
-hydectl theme import --name "Obsidian Purple" --url https://github.com/amit-0i/Obsidian-Purple
+#!/bin/bash
+
+if [ ! -d "$HOME/.config/hyde/themes/Oxo Carbon" ]; then
+  echo "Importing Oxo Carbon..."
+  hydectl theme import --name "Oxo Carbon" --url https://github.com/rishav12s/Oxo-Carbon
+else
+  echo "Skipped Oxo Carbon, already exists."
+fi
+
+if [ ! -d "$HOME/.config/hyde/themes/Crimson Blue" ]; then
+  echo "Importing Crimson Blue..."
+  hydectl theme import --name "Crimson Blue" --url https://github.com/amit-0i/Crimson-Blue
+else
+  echo "Skipped Crimson Blue, already exists."
+fi
+
+if [ ! -d "$HOME/.config/hyde/themes/Eternal Arctic" ]; then
+  echo "Importing Eternal Arctic..."
+  hydectl theme import --name "Eternal Arctic" --url https://github.com/rishav12s/Eternal-Arctic
+else
+  echo "Skipped Eternal Arctic, already exists."
+fi
+
+if [ ! -d "$HOME/.config/hyde/themes/Vanta Black" ]; then
+  echo "Importing Vanta Black..."
+  hydectl theme import --name "Vanta Black" --url https://github.com/rishav12s/Vanta-Black
+else
+  echo "Skipped Vanta Black, already exists."
+fi
+
+if [ ! -d "$HOME/.config/hyde/themes/Obsidian Purple" ]; then
+  echo "Importing Obsidian Purple..."
+  hydectl theme import --name "Obsidian Purple" --url https://github.com/amit-0i/Obsidian-Purple
+else
+  echo "Skipped Obsidian Purple, already exists."
+fi
