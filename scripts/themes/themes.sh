63150a3b4e8a11db97daa3dc4546802adbc4a7d6
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-06-24 23:01:52 +0300

    Update themes.sh

--- scripts/themes/themes.sh
+++ scripts/themes/themes.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.config/hyde/themes/Oxo Carbon" ]; then
-  echo "Importing Oxo Carbon..."
-  hydectl theme import --name "Oxo Carbon" --url https://github.com/rishav12s/Oxo-Carbon
-else
-  echo "Skipped Oxo Carbon, already exists."
-fi
+THEME_DIR="$HOME/.config/hyde/themes"
+WALLPAPER_DIR="./wallpapers"
 
-if [ ! -d "$HOME/.config/hyde/themes/Crimson-Blue" ]; then
-  echo "Importing Crimson Blue..."
-  hydectl theme import --name "Crimson-Blue" --url https://github.com/amit-0i/Crimson-Blue
-else
-  echo "Skipped Crimson Blue, already exists."
-fi
+declare -A THEMES=(
+  ["Oxo Carbon"]="https://github.com/rishav12s/Oxo-Carbon"
+  ["Crimson-Blue"]="https://github.com/amit-0i/Crimson-Blue"
+  ["Eternal Arctic"]="https://github.com/rishav12s/Eternal-Arctic"
+  ["Vanta Black"]="https://github.com/rishav12s/Vanta-Black"
+  ["Obsidian-Purple"]="https://github.com/amit-0i/Obsidian-Purple"
+  ["One Dark"]="https://github.com/RAprogramm/HyDe-Themes/tree/One-Dark"
+  ["Virtual-Witches"]="https://github.com/RAprogramm/HyDe-Themes/tree/Virtual-Witches"
+)
 
-if [ ! -d "$HOME/.config/hyde/themes/Eternal Arctic" ]; then
-  echo "Importing Eternal Arctic..."
-  hydectl theme import --name "Eternal Arctic" --url https://github.com/rishav12s/Eternal-Arctic
-else
-  echo "Skipped Eternal Arctic, already exists."
-fi
+echo "Checking and importing themes..."
+for theme in "${!THEMES[@]}"; do
+  theme_path="$THEME_DIR/$theme"
+  if [[ ! -d "$theme_path" ]]; then
+    echo "Importing $theme..."
+    hydectl theme import --name "$theme" --url "${THEMES[$theme]}"
+  else
+    echo "Skipped $theme, already exists."
+  fi
+done
 
-if [ ! -d "$HOME/.config/hyde/themes/Vanta Black" ]; then
-  echo "Importing Vanta Black..."
-  hydectl theme import --name "Vanta Black" --url https://github.com/rishav12s/Vanta-Black
-else
-  echo "Skipped Vanta Black, already exists."
-fi
+echo "Setting up custom wallpapers..."
 
-if [ ! -d "$HOME/.config/hyde/themes/Obsidian-Purple" ]; then
-  echo "Importing Obsidian Purple..."
-  hydectl theme import --name "Obsidian-Purple" --url https://github.com/amit-0i/Obsidian-Purple
-else
-  echo "Skipped Obsidian Purple, already exists."
-fi
+move_wallpapers() {
+  src="$WALLPAPER_DIR/$1"
+  dest="$THEME_DIR/$2"
 
-if [ ! -d "$HOME/.config/hyde/themes/One Dark" ]; then
-  echo "Importing One Dark..."
-  hydectl theme import --name "One Dark" --url https://github.com/RAprogramm/HyDe-Themes/tree/One-Dark
-else 
+  [[ ! -d "$src" ]] && echo "Source not found: $src" && return
 
-if [ ! -d "$HOME/.config/hyde/themes/Virtual-Witches" ]; then
-  echo "Importing Virtual Witches..."
-  hydectl theme import --name "One Dark" --url https://github.com/RAprogramm/HyDe-Themes/tree/One-Dark
-else 
-  echo "Skipped One Dark, already exists."
+  echo "Getting wallpapers for $2"
+  for wp in "$src"/*; do
+    [[ ! -e "$dest/$(basename "$wp")" ]] && mv "$wp" "$dest/" && echo "Moved: $(basename "$wp")" || echo "Skipped: $(basename "$wp")"
+  done
+}
 
-echo "Finished setting up themes"
+declare -A WALLPAPER_MAP=(
+  ["crimson-blue"]="Crimson-Blue"
+  ["nordic-blue"]="Nordic Blue"
+  ["synth-wave"]="Synth Wave"
+  ["vanta-black"]="Vanta Black"
+  ["oxo-carbon"]="Oxo Carbon"
+  ["graphite-mono"]="Graphite Mono"
+  ["obsidian-purple"]="Obsidian-Purple"
+  ["catpuccin-latte"]="Catpuccin Latte"
+)
+
+for src in "${!WALLPAPER_MAP[@]}"; do
+  move_wallpapers "$src" "${WALLPAPER_MAP[$src]}"
+done
+
+echo "Finished setting up themes."
 exit 0
